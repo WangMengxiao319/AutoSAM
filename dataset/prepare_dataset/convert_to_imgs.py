9e39766edbad159d3c65deababc21c6d7a21e06e
WangMengxiao <mengxiaowang0319@163.com>
2023-11-28 19:43:05 +0800

    李萍老师的CTA数据集，处理成png格式

--- dataset/prepare_dataset/convert_to_imgs.py
+++ dataset/prepare_dataset/convert_to_imgs.py
@@ -4,6 +4,7 @@ import shutil
 from PIL import Image
 from medpy.io import load
 import SimpleITK as sitk
+import nrrd
 
 join = os.path.join
 
@@ -144,11 +145,12 @@ def convert_nii_to_imgs(data_dir, output_dir):
 
 
 def convert_acdc_to_imgs(data_dir, output_dir):
+    '''Mancy 修改，可以直接操作acdc原始数据库'''
     img_dir = join(output_dir, 'imgs')
     label_dir = join(output_dir, 'annotations')
 
     patient_list = os.listdir(data_dir)
-    # patient_list = patient_list[:2]
+    patient_list = patient_list[:2]
     for ppl in patient_list:
         print(ppl)
         files_list = os.listdir(join(data_dir, ppl))
@@ -167,10 +169,11 @@ def convert_acdc_to_imgs(data_dir, output_dir):
                 # data = np.load(os.path.join(data_dir, ppl, f + ".npy"))
                 image = read_nii(join(data_dir, ppl, f + ".nii.gz"))
                 label = read_nii(join(data_dir, ppl, f + "_gt.nii.gz"))
-
+                print(image.shape)
                 for i in range(image.shape[0]):
                     img_array = (image[i] - image.min()) / (image.max() - image.min())
                     img_array = np.clip(img_array * 255, 0, 255).astype('uint8')   # 映射到0到255
+                    print(img_array.shape)
                     label_array = label[i].astype('uint8')
                     # print(np.unique(img_array))
                     # print(np.unique(label_array))
@@ -189,6 +192,77 @@ def convert_acdc_to_imgs(data_dir, output_dir):
                     lb.save(join(target_label_dir, save_files))
 
                 print("finishing saving", f)
+
+def convert_nrrd_to_imgs(data_dir, output_dir):
+    '''load nrrd files and convert to png images'''
+    img_dir = join(output_dir, 'imgs')
+    label_dir = join(output_dir, 'annotations')
+    patient_list = os.listdir(data_dir)
+    patient_list.sort(key=lambda x: int(x)) # 按照序数排列
+    # patient_list = patient_list[12:14]  # debug
+    for ppl in patient_list:
+        print(ppl)
+        if int(ppl) < 10:
+            patient_name = 'patient'+ "_00" + ppl
+        elif 10 <= int(ppl) < 100:
+            patient_name = 'patient' + "_0" + ppl 
+        else:
+            patient_name = 'patient' + "_" + ppl 
+        subfolders_list = os.listdir(join(data_dir, ppl))   # ['原图nrrd文件', '标注文件'] 但是名字不太统一
+        target_img_dir = join(img_dir, ppl)
+        target_label_dir = join(label_dir, ppl)
+        if not os.path.exists(target_img_dir):
+            os.makedirs(join(target_img_dir))
+            os.makedirs(join(target_label_dir))
+            print('Created' + target_img_dir + '...')
+            print('Created' + target_label_dir + '...')
+        for folder in subfolders_list:
+            files_list = os.listdir(join(data_dir, ppl, folder))
+            for f in files_list:
+                if f.split(".")[-1] == 'nrrd':  # 所有nrrd结尾的文件
+                    f = f.split(".")[0]
+                    if "Segmentation" in f:
+                        '''处理annotations(label)文件'''
+                        # data = np.load(os.path.join(data_dir, ppl, f + ".npy"))
+                        label,header = nrrd.read(join(data_dir, ppl, folder, f + ".seg.nrrd"))    # Data Shape: (1120, 1120, 1)
+                        label_array = label[:,:,0].transpose(1,0).astype('uint8')*255
+                            # print(np.unique(img_array))
+                        # print(np.unique(label_array))
+                        frame = f.split("_")[-1]
+                        if int(frame) < 10:
+                            frame_name = 'frame' + "_00" + frame
+                        elif 10 <= int(frame) < 100:
+                            frame_name = 'frame' + "_0" + frame
+                        else:
+                            frame_name = 'frame' + "_" + frame
+                        save_files = patient_name +'_'+ frame_name + ".png"
+                        lb = Image.fromarray(label_array)
+                        lb.save(join(target_label_dir, save_files))
+                    else:
+                        '''处理imgs文件'''
+                        image,header = nrrd.read(join(data_dir, ppl, folder, f + ".nrrd"))  # Data Shape: (3, 1120, 1120, 1)
+                        if len(image.shape) == 4:
+                            image = image[:,:,:,0].transpose(2, 1, 0)  # Data Shape: (1120, 1120, 3)
+                        else:
+                            image = image[:,:,0]
+                            image[image == 32767] = 2168
+                            image = image.transpose(1,0).astype(np.float64)
+                        img_array = (image - image.min()) / (image.max() - image.min())
+                        img_array = np.clip(img_array * 255, 0, 255).astype('uint8')   # 映射到0到255
+                        # print(np.unique(img_array))
+                        frame = f.split("_")[-1]
+                        if int(frame) < 10:
+                            frame_name = 'frame' + "_00" + frame
+                        elif 10 <= int(frame) < 100:
+                            frame_name = 'frame' + "_0" + frame
+                        else:
+                            frame_name = 'frame' + "_" + frame
+                        save_files = patient_name + '_'+frame_name + ".png"
+                        im = Image.fromarray(img_array)
+                        new_p = im.convert('RGB')
+                        new_p.save(join(target_img_dir, save_files))
+                    
+                    print("finishing saving", f)
 
 
 def convert_scribbles_to_imgs(data_dir, output_dir):
@@ -264,6 +338,13 @@ if __name__ == "__main__":
     #         new_slice = slice[0:5] + "_" + slice[5:]
     #         print(new_slice)
     #         os.rename(join(output_dir, f, slice), join(output_dir, f, new_slice))
-    data_dir = 'D:/Filez/dataset/ACDC/raw/training'
-    output_dir = 'D:/Filez/dataset/ACDC/processed'
-    convert_acdc_to_imgs(data_dir, output_dir)
+
+    # ### process ACDC dataset
+    # data_dir = 'D:/Filez/dataset/ACDC/raw/training'
+    # output_dir = 'D:/Filez/dataset/ACDC/processed'
+    # convert_acdc_to_imgs(data_dir, output_dir)
+
+    ### process LiPing_CTA dataset
+    data_dir = 'D:/Filez/dataset/LiPing_multi_modal/CTA dataset/已标注（未植入支架）'
+    output_dir = 'D:/Filez/dataset/LiPing_multi_modal/CTA dataset/processed_without_stent'
+    convert_nrrd_to_imgs(data_dir, output_dir)
